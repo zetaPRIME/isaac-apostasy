340a42c7f144e3c489cc1f9096d5c2b0798b0482
zetaPRIME <qmanxt@gmail.com>
2025-04-14 16:46:01 -0400

    multishot the quick way

--- Apostasy/util/misc.lua
+++ Apostasy/util/misc.lua
@@ -7,6 +7,12 @@ function util.playerDPS(player, useMultishot)
     return player.Damage * fr * ms
 end
 
-function util.playerMultishot() return 1 end -- TODO
+if REPENTOGON then
+    function util.playerMultishot(player)
+        return player:GetMultiShotParams(WeaponType.WEAPON_TEARS):GetNumTears()
+    end
+else
+    function util.playerMultishot() return 1 end -- TODO: non dummy
+end
 
 return util
