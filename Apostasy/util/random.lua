f6f252b27f68d4ca1f2fe3327653bfa74ebc2f94
zetaPRIME <qmanxt@gmail.com>
2025-04-19 16:52:20 -0400

    refactor random lib

--- Apostasy/util/random.lua
+++ Apostasy/util/random.lua
@@ -7,13 +7,13 @@ local function rawFloatRoll()
     return (Random() % 10000) / 10000
 end
 
-function random.rollPercent(chance, luck)
+function rollBase(rndFunc, chance, luck)
     if type(luck) == "userdata" then luck = luck.Luck end
     
-    local roll = rawPercentRoll()
+    local roll = rndFunc()
     -- luck weights for advantage/disadvantage in range -10 .. 10
     if luck and math.abs(luck*10) > rawPercentRoll() then
-        local roll2 = rawPercentRoll()
+        local roll2 = rndFunc()
         if luck > 0 then roll = math.min(roll, roll2)
         else roll = math.max(roll, roll2) end
     end
@@ -21,5 +21,8 @@ function random.rollPercent(chance, luck)
     if not chance then return roll end
     return chance > roll
 end
+
+function random.rollPercent(chance, luck) return rollBase(rawPercentRoll, chance, luck) end
+function random.rollFloat(chance, luck) return rollBase(rawFloatRoll, chance, luck) end
 
 return random
