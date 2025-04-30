5e2ea14a1c23fea793fe2ac658b8a31f405f47b7
zetaPRIME <qmanxt@gmail.com>
2025-04-29 20:18:55 -0400

    start of entitycore

---
+++ Apostasy/modules/itemcore.lua
@@ -0,0 +1,161 @@
+local Apostasy = _ENV["::Apostasy"]
+
+local saveManager = Apostasy:require "lib.save_manager"
+
+local util = Apostasy:require "util.misc"
+local sleep = util.sleep
+
+local Item = { } -- prototype
+
+local byName = { }
+local byIdItems = { }
+local byIdTrinkets = { }
+
+function Apostasy:RegisterItem(name)
+    local itm = setmetatable({ }, { __index = Item })
+    itm.name = name
+    itm.id = Isaac.GetItemIdByName(name)
+    
+    byIdItems[itm.id] = itm
+    byName[name] = itm
+    return itm
+end
+
+function Apostasy:RegisterTrinket(name)
+    local itm = setmetatable({ }, { __index = Item })
+    itm.isTrinket = true
+    itm.name = name
+    itm.id = Isaac.GetTrinketIdByName(name)
+    
+    byIdTrinkets[itm.id] = itm
+    byName[name] = itm
+    return itm
+end
+
+function Apostasy:GetItem(p)
+    local pt = type(p)
+    if pt == "number" then return byIdItems[p] end
+    if pt == "string" then return byName[p] end
+    return nil
+end
+
+function Apostasy:GetTrinket(p)
+    local pt = type(p)
+    if pt == "number" then return byIdTrinkets[p] end
+    if pt == "string" then return byName[p] end
+    return nil
+end
+
+function Item:IsHeldBy(player, real, discrete)
+    if self.isTrinket then return player:HasTrinket(self.id, real) end
+    return player:HasCollectible(self.id, real, discrete)
+end
+
+function Item:NumHeldBy(player, real, discrete)
+    if self.isTrinket then return player:GetTrinketMultiplier(self.id, real) end
+    return player:GetCollectibleNum(self.id, real, discrete)
+end
+
+local forPlayer = setmetatable({ }, { __mode = "k" }) -- weakly keyed
+local updQ = setmetatable({ }, { __mode = "k" })
+
+function Apostasy:UpdateItemsFor(player)
+    local pd = player:GetData()
+    updQ[pd] = nil
+    if not player:AsPlayer() or not player:Exists() then
+        forPlayer[pd] = nil
+    else
+        player = player:AsPlayer()
+        local l = { }
+        for k,itm in byName do
+            if itm:IsHeldBy(player) then l[itm] = [itm] end
+        end
+        forPlayer[pd] = l
+    end
+end
+function Apostasy:QueueUpdateItemsFor(player)
+    local pd = player:GetData()
+    if not updQ[pd] then
+        updQ[pd] = true
+        Apostasy:QueueUpdateRoutine(Apostasy.UpdateItemsFor, self, player)
+    end
+end
+
+
+
+
+
+
+-- handle catching item wisps
+Apostasy:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, function(_, fam)
+    local player = fam.SpawnerEntity:ToPlayer()
+    if not player then return end
+    local itm = fam.SubType
+    local pd = player:GetData()
+    local l = forPlayer[pd]
+    if not l then l = { } forPlayer[pd] = l end
+    l[itm] = [itm]
+end, FamiliarVariant.ITEM_WISP)
+
+Apostasy:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, function(_, ent)
+    if ent.Variant ~= FamiliarVariant.ITEM_WISP then return end
+    Apostasy:QueueUpdateItemsFor(ent.SpawnerEntity:ToPlayer())
+end, EntityType.ENTITY_FAMILIAR)
+
+if REPENTOGON then -- set up our added/removed hooks
+    Apostasy:AddPriorityCallback(ModCallbacks.MC_POST_PLAYER_INIT, CallbackPriority.LATE * 5, Apostasy.QueueUpdateItemsFor)
+    
+    Apostasy:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, function(_, id, charge, firstTime, slot, vdata, player)
+        local itm = byIdItems[id]
+        if itm then
+            local pd = player:GetData()
+            local l = forPlayer[pd]
+            if not l then l = { } forPlayer[pd] = l end
+            l[itm] = [itm]
+        end
+    end)
+    
+    Apostasy:AddCallback(ModCallbacks.MC_POST_TRIGGER_COLLECTIBLE_REMOVED, function(_, player, id)
+        local itm = byIdItems[id]
+        if itm then
+            Apostasy:QueueUpdateItemsFor(player)
+        end
+    end)
+    
+    Apostasy:AddCallback(ModCallbacks.MC_POST_TRIGGER_TRINKET_ADDED, function(_, player, id, firstTime)
+        local itm = byIdTrinkets[id]
+        if itm then
+            local pd = player:GetData()
+            local l = forPlayer[pd]
+            if not l then l = { } forPlayer[pd] = l end
+            l[itm] = [itm]
+        end
+    end)
+    
+    Apostasy:AddCallback(ModCallbacks.MC_POST_TRIGGER_TRINKET_REMOVED, function(_, player, id)
+        local itm = byIdTrinkets[id]
+        if itm then
+            Apostasy:QueueUpdateItemsFor(player)
+        end
+    end)
+else -- we get the Hacky Way for vanilla...
+    local function playerTracking(player)
+        while player:Exists() do
+            Apostasy:UpdateItemsFor(player)
+            sleep(10)
+            if player.QueuedItem then
+                local qi = player.QueuedItem
+                for i = 1, 180 do -- limit sleep time
+                    if not player.QueuedItem then break end
+                    coroutine.yield()
+                end
+                if not player:Exists() then return end
+                -- TODO trigger pickup routine
+            end
+        end
+    end
+    
+    Apostasy:AddPriorityCallback(ModCallbacks.MC_POST_PLAYER_INIT, CallbackPriority.LATE * 5, function(_, player)
+        Apostasy:QueueUpdateRoutine(playerTracking, player)
+    end)
+end
