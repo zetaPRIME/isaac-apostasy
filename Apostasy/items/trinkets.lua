bf768995621bccbb3a095e6ac801d2a1370f6eae
zetaPRIME <qmanxt@gmail.com>
2025-04-30 03:02:10 -0400

    speed doesn't exactly work like that

--- Apostasy/items/trinkets.lua
+++ Apostasy/items/trinkets.lua
@@ -7,6 +7,7 @@ local sfx = SFXManager()
 
 
 local skullmask = Apostasy:RegisterTrinket("Skull Mask")
+skullmask:AddDescription("↑ +0.15 Speed#20% chance to avoid damage while moving quickly#{{Blank}} (10% chance per Luck to roll with advantage)")
 
 function skullmask:OnEvaluateCache(player, cacheFlag)
     if cacheFlag == CacheFlag.CACHE_SPEED then
@@ -18,7 +19,7 @@ function skullmask:OnTakeDamage(e, amount, flags, source, inv)
     local player = e:ToPlayer()
     if flags & DamageFlag.DAMAGE_FAKE > 0 then return end
     
-    if player.Velocity:Length() >= player.MoveSpeed * 3.5 then
+    if player.Velocity:Length() >= 3.5 then
         if rand.rollPercent(20, player) then
             sfx:Play(SoundEffect.SOUND_SWORD_SPIN, 1, 0, false, 1.25)
             sfx:Play(SoundEffect.SOUND_ISAAC_HURT_GRUNT, 0, 3) -- block hurt sound
@@ -26,8 +27,4 @@ function skullmask:OnTakeDamage(e, amount, flags, source, inv)
             return false
         end
     end
-end
-
-if EID then
-    EID:addTrinket(skullmask.id, "↑ +0.15 Speed#20% chance to avoid damage while moving near full speed#{{Blank}} (10% chance per Luck to roll with advantage)")
 end
